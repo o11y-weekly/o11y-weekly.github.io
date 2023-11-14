9abb87f6e07da28c5c272e867ff5268adcff5dee
Clément Boudereau <cboudereau@gmail.com>
2023-11-14 17:15:31 +0100

    setup vector restart to show resumed rate graph

--- 2023-11-16_Meet_Vector/demo/run.sh
+++ 2023-11-16_Meet_Vector/demo/run.sh
@@ -1,2 +1,10 @@
 #! /bin/bash
-docker compose down --remove-orphans -v --rmi local && docker compose up
+docker compose down --remove-orphans -v --rmi local && docker compose up -d
+
+docker compose logs -f &
+
+while true 
+do
+    sleep 30
+    docker compose restart vector
+done
