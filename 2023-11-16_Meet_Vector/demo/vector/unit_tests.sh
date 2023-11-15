b395b64ba8c9bc8a7b57c136cd3c99e09c54953b
Clément Boudereau <cboudereau@gmail.com>
2023-11-15 14:08:33 +0100

    fix

--- 2023-11-16_Meet_Vector/demo/vector/unit_tests.sh
+++ 2023-11-16_Meet_Vector/demo/vector/unit_tests.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if docker run --rm -v $(pwd):/vector/config/ timberio/vector:0.34.0-debian test --config-toml /vector/config/**/*.toml; then
+if docker run --rm -w /vector -v $(pwd):/vector/config/ timberio/vector:0.34.0-debian test --config-toml /vector/config/**/*.toml; then
     echo "all tests ... passed"
 else
     echo "test failed! exit code : $?"
