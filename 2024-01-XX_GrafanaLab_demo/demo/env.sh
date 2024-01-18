9f8dad99dd4f3d99397a50e616ec20d557082c44
Clément Boudereau <cboudereau@gmail.com>
2024-01-18 15:11:00 +0100

    remove otel collector contrib

--- 2024-01-XX_GrafanaLab_demo/demo/env.sh
+++ 2024-01-XX_GrafanaLab_demo/demo/env.sh
@@ -4,5 +4,4 @@ export CONSUL_VERSION=1.17.0
 export JDK_IMAGE=openjdk:17-alpine
 export DISTRO=alpine:3.18.4
 export OTEL_JAVA_AGENT_VERSION=v1.31.0
-export OTEL_CONTRIB_COL=0.88.0
 export VECTOR_VERSION=0.34.0-alpine
