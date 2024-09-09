ce9c96579a74d8e5eb89e9dce064cf001398900c
Clément Boudereau <cboudereau@gmail.com>
2024-09-09 09:37:00 +0200

    feat/java/otel/agentless: add logback otel forwarder and setup

--- 2024-01-31_OpenTelemetry_Looks_Good_To_Me/demo/client/src/main/java/com/demo/client/ClientApplication.java
+++ 2024-01-31_OpenTelemetry_Looks_Good_To_Me/demo/client/src/main/java/com/demo/client/ClientApplication.java
@@ -1,11 +1,16 @@
 package com.demo.client;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
 @EnableFeignClients
+@ConfigurationPropertiesScan("com.demo.client")
+@ImportAutoConfiguration(OpenTelemetryLoggingAutoConfiguration.class)
 public class ClientApplication {
 
 	public static void main(final String[] args) {
