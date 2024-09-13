c748a45f55aef3ffa5028bf687251222f81f807d
Clément Boudereau <cboudereau@gmail.com>
2024-09-13 18:57:49 +0200

    java/otel/hibernate: truncate too long tag value

--- 2024-01-31_OpenTelemetry_Looks_Good_To_Me/demo/service/src/main/java/com/demo/service/AppConfig.java
+++ 2024-01-31_OpenTelemetry_Looks_Good_To_Me/demo/service/src/main/java/com/demo/service/AppConfig.java
@@ -7,13 +7,24 @@ import org.springframework.context.annotation.Configuration;
 
 import io.micrometer.core.instrument.Tags;
 import io.micrometer.core.instrument.binder.MeterBinder;
+import io.micrometer.core.instrument.config.MeterFilter;
 import jakarta.persistence.EntityManagerFactory;
-
 
 @Configuration
 public class AppConfig {
 	@Bean
 	MeterBinder hibernateQueryMetrics(EntityManagerFactory entityManager) {
 		return new HibernateQueryMetrics(entityManager.unwrap(SessionFactoryImplementor.class), "mySess", Tags.empty());
-	}	
+	}
+
+	@Bean
+	public MeterFilter truncateFilter() {
+		return MeterFilter.replaceTagValues("query", tagValue -> {
+			final var maxLength = 1024 - 3;
+			if (tagValue.length() > maxLength) {
+				return String.format("%s...", tagValue.substring(0, maxLength));
+			}
+			return tagValue;
+		});
+	}
 }
