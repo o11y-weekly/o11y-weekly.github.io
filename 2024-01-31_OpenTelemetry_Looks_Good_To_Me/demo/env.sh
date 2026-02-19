c852b28e68d85114dbd2ee5b73d6402e4e825ee5
Clément Boudereau <cboudereau@gmail.com>
2026-02-19 08:25:29 +0100

    fix: update java docker image tag

--- 2024-01-31_OpenTelemetry_Looks_Good_To_Me/demo/env.sh
+++ 2024-01-31_OpenTelemetry_Looks_Good_To_Me/demo/env.sh
@@ -1,6 +1,6 @@
 set -eu
 
-export JDK_IMAGE=openjdk:17-alpine
+export JDK_IMAGE=openjdk:17-ea-alpine
 export DISTRO=alpine:3.19.0
 export OTEL_JAVA_AGENT_VERSION=v2.3.0
 export OTEL_CONTRIB_COL=0.120.0
